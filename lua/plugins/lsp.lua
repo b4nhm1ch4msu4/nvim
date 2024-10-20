65e79115277eb7d5b5785766fd09eb1cac120985
TrangAnh pc ARch <tienpham102vp@gmail.com>
2024-10-20 09:40:12 +0700

    add global vim for diagnostics, change space key -> leader key, change pyright lsp -> pylsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
 
     require('mason-lspconfig').setup({
       -- A list of servers to automatically install if they're not already installed
-      ensure_installed = { 'pyright', 'lua_ls', 'clangd', 'cmake', 'bashls' },
+      ensure_installed = { 'pylsp', 'lua_ls', 'clangd', 'cmake', 'bashls' },
     })
     -- Set different settings for different languages' LSP
     -- LSP list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -30,10 +30,10 @@ return {
     -- Customized on_attach function
     -- See `:help vim.diagnostic.*` for documentation on any of the below functions
     local opts = { noremap = true, silent = true }
-    vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
     -- Use an on_attach function to only map the following keys
     -- after the language server attaches to the current buffer
@@ -75,11 +75,18 @@ return {
     lspconfig.cmake.setup({
       on_attach = on_attach,
     })
-    lspconfig.pyright.setup({
+    lspconfig.pylsp.setup({
       on_attach = on_attach,
     })
     lspconfig.lua_ls.setup({
       on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+        },
+      },
     })
   end
 }
